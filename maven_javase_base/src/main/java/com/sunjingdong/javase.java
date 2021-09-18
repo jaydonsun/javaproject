645892173a34262216885deb6ecc51dae2835976
jaydonsun <jaydonsun0605@gmail.com>
2021-09-19 00:58:30 +0800

    insert javase

---
+++ maven_javase_base/src/main/java/com/sunjingdong/javase.java
@@ -0,0 +1,11 @@
+package com.sunjingdong;
+
+public class javase {
+    public static void main(String[] args) {
+        aaa();
+    }
+    public static void aaa(){
+        System.out.println("你好");
+    }
+}
+
